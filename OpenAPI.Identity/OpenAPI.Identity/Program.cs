7324868ff13684c5e1ea588e2adc9b928ed8604b
Temur Mikava <temsonmikava@gmail.com>
2024-06-22 17:45:08 +0400

    Change rabbitmq credantials

--- OpenAPI.Identity/OpenAPI.Identity/Program.cs
+++ OpenAPI.Identity/OpenAPI.Identity/Program.cs
@@ -33,8 +33,8 @@ builder.Services.AddMassTransit(configuration =>
     {
         cfg.Host("rabbitmq", "/", h =>
         {
-            h.Username("guest");
-            h.Password("guest");           
+            h.Username("tmikava");
+            h.Password("Npottwyctd12");           
         });
     });
 });
